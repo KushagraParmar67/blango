c4a1ed91dd7dae7f7ad004c06e70fb61619e9f14
Kushagra Parmar <2414508351@mujonline.edu.in>
2024-08-15 14:21:48 +0000

    Finish django Allauth

--- blango/urls.py
+++ blango/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("ip/", views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register"),
     path("accounts/", include("django_registration.backends.activation.urls")),
