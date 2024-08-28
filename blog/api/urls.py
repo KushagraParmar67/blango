d61071ee90dc43b5e241c6c4d23a30c5ac51d291
Kushagra Parmar <2414508351@mujonline.edu.in>
2024-08-28 07:37:22 +0000

    finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('tags', TagViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,5 +40,6 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
 # urlpatterns = format_suffix_patterns(urlpatterns)
