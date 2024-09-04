1ccb6fdbadce9d8e674744e9b71b0028d6de4379
Kushagra Parmar <2414508351@mujonline.edu.in>
2024-09-04 06:21:08 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 import logging
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers
+from django.urls import reverse
 # Create your views here.
 
 logger = logging.getLogger(__name__)
@@ -17,7 +18,7 @@ def index(request):
   return render(request,"blog/index.html",{"posts":posts})
 
 def post_table(request):
-  return render(request, "blog/post-table.html")
+  return render(request, "blog/post-table.html",{"post_list_url":reverse("post-list")})
 
 def post_detail(request,slug):
   post = get_object_or_404(Post,slug=slug)
